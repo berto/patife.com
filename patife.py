a57800b1239b08dcf788b81bd14bf1d010e6d1eb
Yurka <_yurka_@inbox.ru>
2015-12-04 07:35:41 +0300

    moving on sqla

--- patife.py
+++ patife.py
@@ -4,12 +4,14 @@
 
 """
 from __builtin__ import unicode
+
 from flask import Flask, request, session, redirect, url_for, abort, \
     render_template, flash, _app_ctx_stack  # Import Flask libraries and features
 from flaskext.mysql import MySQL
 from datetime import datetime, date, time
 # Import flask-mysql. It need to be installed additionally: pip2 install flask-mysql. Only works with Python2 for now
 import MySQLdb.cursors
+from flask_sqlalchemy import SQLAlchemy
 
 # Configuration for our Flask application
 DEBUG = True
@@ -23,48 +25,102 @@ MYSQL_DATABASE_DB = 'test'
 MYSQL_DATABASE_USER = 'root'
 MYSQL_DATABASE_PASSWORD = ''
 
+SQLALCHEMY_DATABASE_URI = 'mysql://root:root@127.0.0.1/test'
+
 # Creating the application
 app = Flask(__name__)
 # Configuring the application using default naming
 app.config.from_object(__name__)
+
+db = SQLAlchemy(app)
 
 # Initializing MySQL application
 mysql = MySQL(cursorclass=MySQLdb.cursors.DictCursor)
 mysql.init_app(app)
 
 
-def get_db():
-    """Opens a new database connection if there is none yet for the
-    current application context.
+class Category(db.Model):
     """
-    top = _app_ctx_stack.top  # Get top element in application context stack
-    if not hasattr(top, 'mysql_db'):  # If we have not connected to DB - connect
-        top.mysql_db = mysql.connect()
-    return top.mysql_db
+    This is blog entry's category
+    """
+    __tablename__ = 'categories'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title_en = db.Column(db.Text, nullable=False)
+    title_pt = db.Column(db.Text, nullable=False)
+    weight = db.Column(db.Integer, nullable=False)
+
+    def __init__(self, title_en, title_pt, weight):
+        self.title_en = title_en
+        self.title_pt = title_pt
+        self.weight = weight
+
+    def __repr__(self):
+        return '<Category {}({})>'.format(self.title_en, self.title_pt)
+
+
+class Entry(db.Model):
+    """
+    This is blog entry's category
+    """
+    __tablename__ = 'entries'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title_en = db.Column(db.Text, nullable=False)
+    title_pt = db.Column(db.Text, nullable=False)
+    text_en = db.Column(db.Text, nullable=False)
+    text_pt = db.Column(db.Text, nullable=False)
+    date_created = db.Column(db.Date, nullable=False, default=date.today)
+    date_updated = db.Column(db.Date, nullable=False, onupdate=date.today)
+    category_id = db.Column(db.Integer)
+
+    category = db.relationship('Category',
+                               backref=db.backref('entries', lazy='dynamic'))
+
+    def __init__(self, title_en, title_pt, text_en, text_pt, category=None):
+        self.title_en = title_en
+        self.title_pt = title_pt
+        self.text_en = text_en
+        self.text_pt = text_pt
+        self.category = category
+
+    def __repr__(self):
+        return '<Entry {}({})>'.format(self.title_en, self.title_pt)
+
+
+# def get_db():
+#     """Opens a new database connection if there is none yet for the
+#     current application context.
+#     """
+#     top = _app_ctx_stack.top  # Get top element in application context stack
+#     if not hasattr(top, 'mysql_db'):  # If we have not connected to DB - connect
+#         top.mysql_db = mysql.connect()
+#     return top.mysql_db
 
 
 def init_db():
     """Creates the database tables."""
-    with app.app_context():
-        db = get_db()
-        request = ""  # Reading schema.sql and setting for request
-        with app.open_resource('schema.sql', mode='r') as f:
-            request += f.read() + " "
-        # Executing request
-        cursor = db.cursor()
-        cursor.execute(request)
-        cursor.close()
-        # Commiting changes to DB
-        db.commit()
+    db.create_all()
+    # with app.app_context():
+    #     db = get_db()
+    #     request = ""  # Reading schema.sql and setting for request
+    #     with app.open_resource('schema.sql', mode='r') as f:
+    #         request += f.read() + " "
+    #     # Executing request
+    #     cursor = db.cursor()
+    #     cursor.execute(request)
+    #     cursor.close()
+    #     # Commiting changes to DB
+    #     db.commit()
 
 
-@app.teardown_appcontext
-def close_db_connection(exception):
-    """Closes the database again at the end of the request."""
-    top = _app_ctx_stack.top  # Get top element in application context stack
-    # If we have a connection to DB - close it
-    if hasattr(top, 'mysql_db'):
-        top.mysql_db.close()
+# @app.teardown_appcontext
+# def close_db_connection(exception):
+#     """Closes the database again at the end of the request."""
+#     top = _app_ctx_stack.top  # Get top element in application context stack
+#     # If we have a connection to DB - close it
+#     if hasattr(top, 'mysql_db'):
+#         top.mysql_db.close()
 
 
 @app.route('/')
@@ -74,38 +130,44 @@ def home_view():
 
 @app.route('/entries/')
 def view_entries():
-    # Getting cursor and selecting all entries from DB
-    cursor = get_db().cursor()
-    cursor.execute('SELECT * FROM entries ORDER BY date_created DESC')
-    entries = cursor.fetchall()
-    cursor.execute('SELECT * FROM categories ORDER BY weight DESC')
-    categories = cursor.fetchall()
-    # MySQL need cursors to be closed immediately after we execute request
-    cursor.close()
+    # # Getting cursor and selecting all entries from DB
+    # cursor = get_db().cursor()
+    # cursor.execute('SELECT * FROM entries ORDER BY date_created DESC')
+    # entries = cursor.fetchall()
+    # cursor.execute('SELECT * FROM categories ORDER BY weight DESC')
+    # categories = cursor.fetchall()
+    # # MySQL need cursors to be closed immediately after we execute request
+    # cursor.close()
+    entries = Entry.query.order_by(Entry.date_created).all()
+    categories = Category.query.order_by(Category.weight).all()
     # Rendering index page
-    return render_template('entry_read_all.html', entries=entries, categories=categories)
+    return render_template('entry_read_all.html',
+                           entries=entries, categories=categories)
 
 
-@app.route('/entries/<int:id>/')
-def view_entry(id):
-    # Getting cursor and selecting entry from DB
-    cursor = get_db().cursor()
-    # we use * to get all fields from entries, otherwise we'd have to call them one by one
-    query = "SELECT * FROM entries WHERE id={0}".format(id)
-    cursor.execute(query)
-    entry = cursor.fetchone()
-    cursor.close()
+@app.route('/entries/<int:entry_id>/')
+def view_entry(entry_id):
+    # # Getting cursor and selecting entry from DB
+    # cursor = get_db().cursor()
+    # # we use * to get all fields from entries, otherwise we'd have to call them one by one
+    # query = "SELECT * FROM entries WHERE id={0}".format(id)
+    # cursor.execute(query)
+    # entry = cursor.fetchone()
+    # cursor.close()
     # Rendering edit page
+    entry = Entry.query.get(entry_id)
     return render_template('entry_read.html', entry=entry)
 
 
 @app.route('/entries/new')
 def new_entry():
-    cursor = get_db().cursor()
-    query = "SELECT * FROM categories ORDER BY weight DESC".format(id)
-    cursor.execute(query)
-    categories = cursor.fetchall()
-    cursor.close()
+    # cursor = get_db().cursor()
+    # query = "SELECT * FROM categories ORDER BY weight DESC".format(id)
+    # cursor.execute(query)
+    # categories = cursor.fetchall()
+    # cursor.close()
+
+    categories = Category.query.order_by(Category.weight).all()
 
     # just displaying the blank template to add
     return render_template('entry_create.html', categories=categories, current_time=date.today())
@@ -133,18 +195,21 @@ def add_entry():
     return redirect(url_for('home_view'))
 
 
-@app.route('/entries/<int:id>/edit')
-def edit_entry(id):
+@app.route('/entries/<int:entry_id>/edit')
+def edit_entry(entry_id):
     # Getting cursor and selecting entry for edit from DB
-    cursor = get_db().cursor()
-    # we use * to get all fields from entries, otherwise we'd have to call them one by one
-    query = "SELECT * FROM entries WHERE id={0}".format(id)
-    cursor.execute(query)
-    entry = cursor.fetchone()
-    query = "SELECT * FROM categories ORDER BY weight DESC"
-    cursor.execute(query)
-    categories = cursor.fetchall()
-    cursor.close()
+    # cursor = get_db().cursor()
+    # # we use * to get all fields from entries, otherwise we'd have to call them one by one
+    # query = "SELECT * FROM entries WHERE id={0}".format(id)
+    # cursor.execute(query)
+    # entry = cursor.fetchone()
+    # query = "SELECT * FROM categories ORDER BY weight DESC"
+    # cursor.execute(query)
+    # categories = cursor.fetchall()
+    # cursor.close()
+
+    entry = Entry.query.get(entry_id)
+    categories = Category.query.order_by(Category.weight).all()
     # Rendering edit page
     return render_template('entry_update_and_delete.html', entry=entry, categories=categories,
                            current_time=date.today())
